06ff43b68d79c52b1eda3088484a84561f6a4704
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-02-11 19:23:01 +0200

    Create base user model

---
+++ server/bundles/UserBundle/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    min: 6,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+userSchema.statics.findByName = (searchPhrase, callback) =>
+  this.findOne(
+    {
+      firsName: new RegExp(searchPhrase, 'i'),
+      lastName: new RegExp(searchPhrase, 'i'),
+    },
+    callback,
+  );
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
