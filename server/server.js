bf73ba1bc491f72550a20fab881bc8a18e865c30
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-02-11 19:22:49 +0200

    Add test get api url

--- server/server.js
+++ server/server.js
@@ -5,13 +5,25 @@ import path from 'path';
 import { PORT } from './app/config/server.config';
 import { connectDb } from './app/db/db';
 
+// Test features
+import User from './bundles/UserBundle/models/User';
+
 const app = express();
 
-app.use('/*', express.static(path.resolve(__dirname, '../public/index.html')));
+// app.use('/*', express.static(path.resolve(__dirname, '../public/index.html')));
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// TODO: Replace to UserBundle controllers
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) res.status(500).send('Error occurred when try to find users');
+
+    res.send(users);
+  });
+});
 
 const startServer = () => {
   app.listen(process.env.PORT || PORT, () => {
