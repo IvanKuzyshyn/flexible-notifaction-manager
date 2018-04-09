99123da835f69ee49bc2043794b37ab038184bf6
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-04-09 18:29:13 +0300

    Update sign in component

--- client/src/bundles/AuthenticationBundle/components/SignInForm.js
+++ client/src/bundles/AuthenticationBundle/components/SignInForm.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField, Button } from 'material-ui';
+import { Link } from 'react-router-dom';
+import Card, { CardContent, CardActions } from 'material-ui/Card';
+import Divider from 'material-ui/Divider';
+
+import { SIGN_UP_ROUTE } from '../constants/routes';
 
 const SignInForm = ({ onSubmit, onChange }) => (
   <div className="sign-in-form">
-    <TextField
-      label="Email"
-      onChange={onChange('email')}
-      variant="email"
-      fullWidth
-    />
-    <TextField
-      label="Password"
-      onChange={onChange('password')}
-      variant="password"
-      fullWidth
-    />
-    <Button variant="raised" color="primary" onClick={onSubmit}>
-      Sign in
-    </Button>
+    <Card>
+      <CardContent>
+        <TextField
+          label="Email"
+          onChange={onChange('email')}
+          variant="email"
+          fullWidth
+        />
+        <TextField
+          label="Password"
+          onChange={onChange('password')}
+          variant="password"
+          fullWidth
+        />
+      </CardContent>
+      <CardActions>
+        <Button variant="raised" color="primary" onClick={onSubmit}>
+          Sign in
+        </Button>
+        <Divider />
+        Already have an account?
+        <Link to={SIGN_UP_ROUTE}>
+          <Button color="secondary">Register</Button>
+        </Link>
+      </CardActions>
+    </Card>
   </div>
 );
 
