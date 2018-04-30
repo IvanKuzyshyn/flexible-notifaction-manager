9376cda55b830d0df42064f757404f0a95607a6b
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-04-30 16:03:46 +0300

    Update sign in component & container

--- client/src/bundles/AuthenticationBundle/components/SignInForm.js
+++ client/src/bundles/AuthenticationBundle/components/SignInForm.js
@@ -1,40 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TextField, Button } from 'material-ui';
+import { Button } from 'material-ui';
 import { Link } from 'react-router-dom';
 import Card, { CardContent, CardActions } from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import { LinearProgress } from 'material-ui/Progress';
+import { FormControlLabel } from 'material-ui/Form';
+import { Field, Form } from 'react-final-form';
 
 import { SIGN_UP_ROUTE } from '../constants/routes';
+import TextField from '../../CommonBundle/components/forms/TextField';
+import Checkbox from '../../CommonBundle/components/forms/Checkbox';
+import { composeValidators } from '../../CommonBundle/validators/utils';
+import {
+  isRequired,
+  mustBeEmail,
+  minLimit,
+} from '../../CommonBundle/validators/fields';
 
-const SignInForm = ({ onSubmit, onChange, isSigningIn }) => (
+const SignInForm = ({ onSubmit, isSigningIn }) => (
   <div className="sign-in-form">
     <Card>
-      <CardContent>
-        <TextField
-          label="Email"
-          onChange={onChange('email')}
-          variant="email"
-          fullWidth
-        />
-        <TextField
-          label="Password"
-          onChange={onChange('password')}
-          variant="password"
-          fullWidth
-        />
-      </CardContent>
-      <CardActions>
-        <Button variant="raised" color="primary" onClick={onSubmit}>
-          Sign in
-        </Button>
-        <Divider />
-        Already have an account?
-        <Link to={SIGN_UP_ROUTE}>
-          <Button color="secondary">Register</Button>
-        </Link>
-      </CardActions>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{ email: '', password: '', remember: true }}
+        render={({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit}>
+            <CardContent>
+              <Field
+                name="email"
+                component={TextField}
+                type="email"
+                placeholder="Email"
+                validate={composeValidators(isRequired, mustBeEmail)}
+                fullWidth
+              />
+              <Field
+                name="password"
+                component={TextField}
+                type="password"
+                placeholder="Password"
+                validate={composeValidators(isRequired, minLimit(8))}
+                fullWidth
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                variant="raised"
+                color="primary"
+                type="submit"
+                disabled={submitting || isSigningIn}
+              >
+                Sign in
+              </Button>
+              <FormControlLabel
+                control={
+                  <Field name="remember" component={Checkbox} type="checkbox" />
+                }
+                label="Remember me"
+              />
+            </CardActions>
+          </form>
+        )}
+      />
+      <Divider />
+      Already have an account?
+      <Link to={SIGN_UP_ROUTE}>
+        <Button color="secondary">Sign up</Button>
+      </Link>
       {isSigningIn && <LinearProgress />}
     </Card>
   </div>
@@ -42,7 +75,6 @@ const SignInForm = ({ onSubmit, onChange, isSigningIn }) => (
 
 SignInForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
   isSigningIn: PropTypes.bool.isRequired,
 };
 
--- client/src/bundles/AuthenticationBundle/containers/SignInContainer.js
+++ client/src/bundles/AuthenticationBundle/containers/SignInContainer.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux';
+import { bindActionCreators } from 'redux';
 
-import { userSignInAction } from "../reducers/authenticationReducer";
+import { userSignInAction } from '../reducers/authenticationReducer';
 import SignInForm from '../components/SignInForm';
 
 class SignInContainer extends Component {
@@ -12,16 +12,10 @@ class SignInContainer extends Component {
     authenticating: PropTypes.bool.isRequired,
   };
 
-  handleSubmitForm = () => {
+  handleSubmitForm = form => {
     const { userSignInAction: signIn } = this.props;
 
-    signIn(this.state);
-  };
-
-  handleChangeAnyFormField = fieldName => event => {
-    this.setState({
-      [fieldName]: event.target.value,
-    });
+    signIn(form);
   };
 
   render() {
@@ -30,7 +24,6 @@ class SignInContainer extends Component {
     return (
       <SignInForm
         onSubmit={this.handleSubmitForm}
-        onChange={this.handleChangeAnyFormField}
         isSigningIn={authenticating}
       />
     );
@@ -40,6 +33,7 @@ class SignInContainer extends Component {
 const mapStateToProps = ({ authentication }) => ({
   authenticating: authentication.authenticating,
 });
-const mapDispatchToProps = dispatch => bindActionCreators({ userSignInAction }, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ userSignInAction }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignInContainer);
