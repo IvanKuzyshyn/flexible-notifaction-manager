d4af10d1ffe5cbae93bf8d79d7c11e67183f11de
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-02 13:07:42 +0300

    Update sign up components

--- client/src/bundles/AuthenticationBundle/components/SignUpForm.js
+++ client/src/bundles/AuthenticationBundle/components/SignUpForm.js
@@ -4,6 +4,10 @@ import Button from 'material-ui/Button';
 import Divider from 'material-ui/es/Divider/Divider';
 import { Link } from 'react-router-dom';
 import { Field, Form } from 'react-final-form';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+import { LinearProgress } from 'material-ui/Progress';
+import Card, { CardContent, CardActions } from 'material-ui/Card';
 
 import { SIGN_IN_ROUTE } from '../constants/routes';
 import TextField from '../../CommonBundle/components/forms/TextField';
@@ -13,65 +17,81 @@ import {
   mustBeEmail,
   minLimit,
 } from '../../CommonBundle/validators/fields';
+import { errorResponseType } from '../../CommonBundle/prop-types/response-types';
 
-const RegistrationForm = ({ onSubmit, onValidate }) => (
+const RegistrationForm = ({ onSubmit, onValidate, isSigningUp, error }) => (
   <div className="sign-up-form">
-    <Form
-      onSubmit={onSubmit}
-      validate={onValidate}
-      render={({ handleSubmit, submitting }) => (
-        <form onSubmit={handleSubmit}>
-          <Field
-            name="firstName"
-            component={TextField}
-            type="text"
-            placeholder="Enter your first name"
-            validate={isRequired}
-            fullWidth
-          />
-          <Field
-            name="lastName"
-            component={TextField}
-            type="text"
-            placeholder="Enter your last name"
-            validate={isRequired}
-            fullWidth
-          />
-          <Field
-            name="email"
-            component={TextField}
-            type="email"
-            placeholder="Enter your email"
-            validate={composeValidators(isRequired, mustBeEmail)}
-            fullWidth
-          />
-          <Field
-            name="password"
-            component={TextField}
-            type="password"
-            placeholder="Enter password"
-            validate={composeValidators(isRequired, minLimit(8))}
-            fullWidth
-          />
-          <Field
-            name="confirmPassword"
-            component={TextField}
-            type="password"
-            placeholder="Confirm your password"
-            validate={composeValidators(isRequired, minLimit(8))}
-            fullWidth
-          />
-          <Button
-            variant="raised"
-            color="primary"
-            type="submit"
-            disabled={submitting}
-          >
-            Sign up
-          </Button>
-        </form>
-      )}
-    />
+    <Card>
+      <Form
+        onSubmit={onSubmit}
+        validate={onValidate}
+        render={({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit}>
+            <CardContent>
+              <Field
+                name="firstName"
+                component={TextField}
+                type="text"
+                placeholder="Enter your first name"
+                validate={isRequired}
+                fullWidth
+              />
+              <Field
+                name="lastName"
+                component={TextField}
+                type="text"
+                placeholder="Enter your last name"
+                validate={isRequired}
+                fullWidth
+              />
+              <Field
+                name="email"
+                component={TextField}
+                type="email"
+                placeholder="Enter your email"
+                validate={composeValidators(isRequired, mustBeEmail)}
+                fullWidth
+              />
+              <Field
+                name="password"
+                component={TextField}
+                type="password"
+                placeholder="Enter password"
+                validate={composeValidators(isRequired, minLimit(8))}
+                fullWidth
+              />
+              <Field
+                name="confirmPassword"
+                component={TextField}
+                type="password"
+                placeholder="Confirm your password"
+                validate={composeValidators(isRequired, minLimit(8))}
+                fullWidth
+              />
+
+              {error && (
+                <Paper elevation={4}>
+                  <Typography variant="title" gutterBottom>
+                    {error.message}
+                  </Typography>
+                </Paper>
+              )}
+            </CardContent>
+            <CardActions>
+              <Button
+                variant="raised"
+                color="primary"
+                type="submit"
+                disabled={submitting}
+              >
+                Sign up
+              </Button>
+            </CardActions>
+          </form>
+        )}
+      />
+      {isSigningUp && <LinearProgress />}
+    </Card>
     <Divider />
     <Link to={SIGN_IN_ROUTE}>
       <Button color="secondary">Sign in</Button>
@@ -82,6 +102,12 @@ const RegistrationForm = ({ onSubmit, onValidate }) => (
 RegistrationForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   onValidate: PropTypes.func.isRequired,
+  isSigningUp: PropTypes.bool.isRequired,
+  error: errorResponseType,
+};
+
+RegistrationForm.defaultProps = {
+  error: null,
 };
 
 export default RegistrationForm;
--- client/src/bundles/AuthenticationBundle/containers/SignUpContainer.js
+++ client/src/bundles/AuthenticationBundle/containers/SignUpContainer.js
@@ -5,10 +5,18 @@ import PropTypes from 'prop-types';
 
 import RegistrationForm from '../components/SignUpForm';
 import { userSignUpAction } from '../reducers/authenticationReducer';
+import { errorResponseType } from '../../CommonBundle/prop-types/response-types';
+import { signUpDataSelector } from '../selectors/authenticationSelectors';
 
 class RegistrationContainer extends Component {
   static propTypes = {
     userSignUpAction: PropTypes.func.isRequired,
+    isProcessing: PropTypes.bool.isRequired,
+    error: errorResponseType,
+  };
+
+  static defaultProps = {
+    error: null,
   };
 
   handleSubmitForm = form => {
@@ -29,18 +37,20 @@ class RegistrationContainer extends Component {
   };
 
   render() {
+    const { isProcessing, error } = this.props;
+
     return (
       <RegistrationForm
         onSubmit={this.handleSubmitForm}
         onValidate={this.handleValidateForm}
+        isSigningUp={isProcessing}
+        error={error}
       />
     );
   }
 }
 
-const mapStateToProps = ({ authentication }) => ({
-  registering: authentication.registering,
-});
+const mapStateToProps = (state) => signUpDataSelector(state);
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ userSignUpAction }, dispatch);
 
