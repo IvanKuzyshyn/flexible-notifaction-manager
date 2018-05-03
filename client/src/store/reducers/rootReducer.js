93a7d2068760f560815c9547a6e454e2ba4bcd0f
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-03 15:31:24 +0300

    Attach user reducer

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import authentication from '../../bundles/AuthenticationBundle/reducers/authenticationReducer';
+import user from '../../bundles/UserBundle/reducers/userReducer';
 
 export default combineReducers({
   authentication,
+  user,
 });
