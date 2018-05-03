9bb8de34a9d97b3e1cc215f7bf401a4b40af759e
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-03 16:38:56 +0300

    User NotAuthenticatedRoute instead of default Route

--- client/src/bundles/AuthenticationBundle/routes/index.js
+++ client/src/bundles/AuthenticationBundle/routes/index.js
@@ -1,15 +1,22 @@
 import React, { Fragment } from 'react';
-import { Route, Redirect, Switch } from 'react-router';
+import { Switch } from 'react-router';
 
 import * as routes from '../constants/routes';
 import SignUpContainer from '../containers/SignUpContainer';
 import SignInContainer from '../containers/SignInContainer';
+import NotAuthenticatedRoute from '../components/router/NotAuthenticatedRoute';
 
 const Routes = () => (
   <Fragment>
     <Switch>
-      <Route path={routes.SIGN_IN_ROUTE} component={SignInContainer} />
-      <Route path={routes.SIGN_UP_ROUTE} component={SignUpContainer} />
+      <NotAuthenticatedRoute
+        path={routes.SIGN_IN_ROUTE}
+        component={SignInContainer}
+      />
+      <NotAuthenticatedRoute
+        path={routes.SIGN_UP_ROUTE}
+        component={SignUpContainer}
+      />
     </Switch>
   </Fragment>
 );
