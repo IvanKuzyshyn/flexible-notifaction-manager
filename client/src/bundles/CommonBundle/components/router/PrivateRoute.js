3c7d48041a1d1e67172e0a2127ba8ffed15f13b8
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-03 15:28:21 +0300

    Create PrivateRoute component

---
+++ client/src/bundles/CommonBundle/components/router/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+import { SIGN_IN_ROUTE } from '../../../AuthenticationBundle/constants/routes';
+import AuthenticationGuard from '../../../AuthenticationBundle/guards/AuthenticationGuard';
+import { Consumer } from '../../../../context/ApplicationContext';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  /* eslint-disable react/prop-types */
+  <Consumer>
+    {({ user }) => (
+      <Route
+        {...rest}
+        render={(...props) =>
+          AuthenticationGuard.can(user) ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to={{ pathname: SIGN_IN_ROUTE, from: props.location }} />
+          )
+        }
+      />
+    )}
+  </Consumer>
+);
+
+PrivateRoute.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
+    .isRequired,
+};
+
+export default PrivateRoute;
