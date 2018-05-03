5cf4408e9bd807588b7d3d0e7fd055bf92dddf8c
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-03 15:44:18 +0300

    Implement store persist engine

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
 
 import rootReducer from './reducers/rootReducer';
 import rootSaga from './sagas/rootSaga';
+import { storagePersistConfig } from '../config/storagePersist';
 
 const sagaMiddleware = createSagaMiddleware();
-
 // eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = composeEnhancers(applyMiddleware(sagaMiddleware));
-
-const store = createStore(rootReducer, middleware);
+const persistedReducer = persistReducer(storagePersistConfig, rootReducer);
+const store = createStore(persistedReducer, middleware);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
