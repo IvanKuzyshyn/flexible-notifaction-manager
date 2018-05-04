bd4bd9dfa5192def344f6e488f90a19b773d7842
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-04 15:23:13 +0300

    Base UserBoard component

--- client/src/bundles/UserBundle/components/UserBoard.js
+++ client/src/bundles/UserBundle/components/UserBoard.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const UserBoard = () => <div>Here is USER BOARD</div>;
+import LayoutContainer from '../../CommonBundle/containers/layout/LayoutContainer';
+
+const UserBoard = () => (
+  <LayoutContainer title="Notifications board">
+    <h1>This is dashboard</h1>
+  </LayoutContainer>
+);
 
 export default UserBoard;
