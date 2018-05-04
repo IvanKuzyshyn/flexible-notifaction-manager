da11944e736d0d2f3830d8985698420d284b5eb0
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-04 17:14:42 +0300

    NotificationBundle: new notification component

---
+++ client/src/bundles/NotificationBundle/components/NewNotification.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import LayoutContainer from '../../CommonBundle/containers/layout/LayoutContainer';
+
+const NewNotification = () => (
+  <LayoutContainer title="Create new notification">
+    <div>New notification component TODO!</div>
+  </LayoutContainer>
+);
+
+export default NewNotification;
---
+++ client/src/bundles/NotificationBundle/containers/NewNotificationContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import NewNotification from '../components/NewNotification';
+
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewNotification);
