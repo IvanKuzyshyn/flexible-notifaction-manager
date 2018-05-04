02cf2fbc48d0e224eb8cefb86cb8147bd2738099
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-04 17:15:30 +0300

    NotificationBundle: notifications list components

---
+++ client/src/bundles/NotificationBundle/components/NotificationsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import LayoutContainer from '../../CommonBundle/containers/layout/LayoutContainer';
+
+const NotificationsList = () => (
+  <LayoutContainer title="Notifications list">
+    <div>Notifications list component TODO!</div>
+  </LayoutContainer>
+);
+
+export default NotificationsList;
---
+++ client/src/bundles/NotificationBundle/containers/NotificationsListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import NotificationsList from '../components/NotificationsList';
+
+const mapStateToProps = state => ({});
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NotificationsList);
