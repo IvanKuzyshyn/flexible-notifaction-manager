12fd32598e35f77a3e101743d71388464a79aa4d
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2018-05-08 16:36:06 +0300

    Use isAuthenticated middleware

--- server/bundles/NotificationBundle/routes/api.js
+++ server/bundles/NotificationBundle/routes/api.js
@@ -2,11 +2,12 @@ import express from 'express';
 import Response from '../../CoreBundle/services/Response';
 
 import Notification from '../models/Notification';
+import isAuthenticated from '../../SecurityBundle/middleware/isAuthenticated';
 
 const router = express.Router();
 
 export default function() {
-  router.get('/api/notifications', async (req, res, next) => {
+  router.get('/api/notifications', isAuthenticated, async (req, res, next) => {
     try {
       const notifications = await Notification.find({})
         .select('message engine notifyAt updatedAt')
